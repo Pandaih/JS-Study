cf5d48a6b9c8fbf51a92eacafdf20618cae8eff1
Pandaih <1287312599@qq.com>
2020-05-19 11:43:35 +0800

    黑马程序员学习了循环控制，for、while、do while

--- 8 for循环/main.js
+++ 8 for循环/main.js
@@ -14,24 +14,24 @@
     想起for结构的1 2 3 4 顺序，1只执行一次，才到 2 3 4；
     当不满足条件的时候，从 2 跳出来了。
 */
-//
-// var result = ['baidu','sohu','taobao','ali'];
-// for (var i = 0; i < 4; i++) {
-//   console.log(result[3]);
-// }
+
+var result = ['baidu','sohu','taobao','ali'];
+for (var i = 0; i < 4; i++) {
+  console.log(result[3]);
+}
 
 // 练习1:1-100的累加和
 var sum1=0;
-for (var i1 = 1; i1 <= 100; i1++) {
-  sum1 += i1;
+for (var a = 1; a <= 100; a++) {
+  sum1 += a;
 }
 console.log(sum1);
 
 // 练习2：求1-100之间所有数的平均值
 var sum2= 0;
 var average= 0;
-for (var i2 = 0; i2 <=100; i2++) {
-  sum2+=i2;
+for (var b = 0; b <=100; b++) {
+  sum2+=b;
 }
 average=sum2/100;
 console.log(average);
@@ -39,11 +39,11 @@ console.log(average);
 // 练习3：求1-100之间所有偶数和、奇数和
 var double= 0;
 var single= 0;
-for (var i3 = 0; i3 <=100; i3++) {
-  if (i3%2 == 0) {
-    double+=i3;
+for (var d = 0; d <=100; d++) {
+  if (d%2 == 0) {
+    double+=d;
   }else {
-    single+=i3;
+    single+=d;
   }
 }
 console.log('1-100之间偶数和：'+double);
@@ -51,9 +51,9 @@ console.log('1-100之间奇数和：'+single);
 
 // 练习 4：求1-100之间所有能被3整除的数字的和
 var sum3= 0;
-for (var i4 = 0; i4 <=100; i4++) {
-      if (i4%3==0) {
-        sum3+=i4;
+for (var e = 0; e <=100; e++) {
+      if (e%3==0) {
+        sum3+=e;
       }
 }
 console.log('1-100之间能被3整除的数字的和：'+sum3);
@@ -62,9 +62,9 @@ console.log('1-100之间能被3整除的数字的和：'+sum3);
 var sum4=0;
 var aver=0;
 var num1=prompt('请输入班级总人数：');
-for (var a = 1; a <=num1; a++) {
-  var b=parseFloat(prompt('请输入第'+a+'个同学的成绩'));
-   sum4+=b;
+for (var f = 1; f <=num1; f++) {
+  var g=parseFloat(prompt('请输入第'+f+'个同学的成绩'));
+   sum4+=g;
   aver=sum4/num1;
 }
 alert('班级总分为：'+sum4);
@@ -76,3 +76,55 @@ for (var r = 1; r <= 5; r++) {
   str=str+'★';
 }
 console.log(str);
+
+// 练习7：打印五行五列星星
+var str2 ='';
+for(var i=1;i<=5;i++){  // 外循环负责打印五行
+  for(var j=1;j<=5;j++){  // 内循环负责打印一行打印五个星星
+    str2=str2+'★';
+  }
+  str2=str2+'\n';
+}
+    console.log(str2);
+
+//练习8：打印n行n列
+var str5 ='';
+var s= prompt('请输入打印的行数：');
+var t= prompt('请输入打印的列数：');
+for(var p =1;p <=s;p++){
+  for(var q=1;q<=t;q++){
+    str5=str5+'★';
+  }
+  str5=str5+'\n';
+}
+console.log(str5);
+
+// 练习9：打印倒三角形
+var str3='';
+for(var k=1;k<=10;k++){
+  for(var l=k;l<=10;l++){
+    str3=str3+'★';
+  }
+  str3=str3+'\n';
+}
+console.log(str3);
+
+// 练习10：打印正三角形
+var str4='';
+for(var k=1;k<=10;k++){
+  for(var l=1;l<=k;l++){
+    str4=str4+'★';
+  }
+  str4=str4+'\n';
+}
+console.log(str4);
+
+// 练习11：打印九九乘法表
+var str6='';
+for(var x=1;x<=9;x++){
+  for(var y=1;y<=x;y++){
+    str6=str6+ y +'x'+ x + '=' + x*y + '\t';
+  }
+  str6= str6 +'\n';
+}
+console.log(str6);
--- 9 while循环/main.js
+++ 9 while循环/main.js
@@ -1,6 +1,73 @@
+/*
+while 循环
+1. 结构：
+while(条件表达式){
+  循环体
+}
+*/
+
 var i= 0;
 while (i<9) {
-  i++;
   console.log(i);
+  i++;  // 如果没有这个语句，会死循环
 }
 console.log('loop stopped');
+
+ // 练习 1: 打印人的一生 1-100岁
+ var a=1;
+ while(a<=100){
+   console.log('小妹现在'+a+'岁');
+   a++;
+ }
+
+ // 练习2：计算1~100の整数和
+ var b=1;
+ var sum=0;
+ while (b<=100) {
+   sum+=b;
+   b++;
+ }
+ console.log(sum);
+
+ // 练习3：弹出一个提示框：你爱我吗？如果答案不是我爱你，一直弹出来
+ var c=prompt('你爱我吗？');
+ while (c !=='我爱你') {
+   c=prompt('你爱我吗？');
+ }
+ alert('我也爱你呀');
+
+/* do while 循环
+1. 结构：
+do{
+  循环体
+}while(条件表达式)
+2. do while 和 while 的区别：
+while 语句先判断表达式的值，值为true时，再执行循环体；
+do while 语句先执行循环体，然后再判断表达式的值；
+3.大多数情况下，do while 和 while 的结果是一样的。
+只有当while的值为false时，结果就不一样。
+do while 至少执行一次循环体，但是 while 一次都不执行
+*/
+
+// 练习4：do while 打印人的一生
+var d= 1;
+do{
+  console.log('我今年'+d+'岁了');
+  d++;
+}while (d<=100)
+
+ // 练习5：do while计算1~100の整数和
+ var sum2=0;
+ var e= 1;
+ do{
+   sum2 += e;
+   e++;
+ }
+ while(e<=100);
+ console.log(sum2);
+
+ // 练习6：do while弹出一个提示框：你爱我吗？如果答案不是我爱你，一直弹出来
+do{
+  var f=prompt('你爱我吗？');
+}while (c !=='我爱你');
+alert('我也爱你呀');
